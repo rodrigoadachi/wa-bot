18a81f4572d50f95fa79c40e66d39b6b9242bf47
Rodrigo Adachi <adachi.rodrigo@gmail.com>
2022-09-25 00:09:16 -0300

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -36,6 +36,7 @@ nvm install v16.17.1
 printf "Install ${RED}Yarn${NC}\n"
 $CURL -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update
 sudo apt install yarn -y
 echo 'export PATH="$PATH:$(yarn global bin)"' >> ~/.bash_profile
 
